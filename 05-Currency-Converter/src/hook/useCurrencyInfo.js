a7570086db41d723f11d9cdf9a30a71e44f4e57d
Ahmad Siddique <asshikrani66@gmail.com>
2025-10-08 00:43:59 +0500

    fixed a little bug in a js file.

--- 05-Currency-Converter/src/hook/useCurrencyInfo.js
+++ 05-Currency-Converter/src/hook/useCurrencyInfo.js
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({});
+    const [data, setData] = useState({})
     useEffect(() => {
-     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-10-06/v1/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then(() => {setData(res[currency])})
-    }, [currency])
-    console.log("Data : ", data);
+      fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-10-06/v1/currencies/${currency}.json`)
+      .then((res) => res.json()) 
+      .then((data) => setData(data[currency]))
+    }, [ currency])
+    
     return data;
 }
 
