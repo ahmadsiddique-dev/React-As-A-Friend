89cc128e816aa6aaac3621b2b01e794e3d25820a
Ahmad Siddique <asshikrani66@gmail.com>
2025-10-11 21:14:07 +0500

    TodoApp Completed

--- 10-TodoApp/src/components/TodoForm.jsx
+++ 10-TodoApp/src/components/TodoForm.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTodo } from '../context';
+
 
 function TodoForm() {
     const [todo, setTodo] = useState("")
@@ -7,14 +8,17 @@ function TodoForm() {
 
     const add = (e) => {
         e.preventDefault();
-        
+        addTodo({ todo, completed : false})
+        setTodo("")
     }
     return (
-        <form  className="flex">
+        <form onSubmit={add} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
--- 10-TodoApp/src/components/TodoItem.jsx
+++ 10-TodoApp/src/components/TodoItem.jsx
@@ -1,36 +1,19 @@
-# todo context api notes
+import React, { useState } from 'react'
+import { useTodo } from '../context';
 
-## Todo Form UI
-
-```javascript
-
-function TodoForm() {
-    
-
-    return (
-        <form  className="flex">
-            <input
-                type="text"
-                placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-            />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
-                Add
-            </button>
-        </form>
-    );
-}
-
-export default TodoForm;
-
-
-```
-
-## Todo Item UI
-
-```javascript
 function TodoItem({ todo }) {
-    
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+    const {updateTodo, deleteTodo, toggleComplete}  = useTodo();
+
+    const editTodo = () => {
+        updateTodo(todo.id, {...todo, todo : todoMsg})
+        setIsTodoEditable(false);
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
 
     return (
         <div
@@ -79,21 +62,3 @@ function TodoItem({ todo }) {
 }
 
 export default TodoItem;
-
-```
-
-## App js UI
-
-```javascript
-<div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4">
-                        {/* Todo form goes here */} 
-                    </div>
-                    <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
-                    </div>
-                </div>
-            </div>
-```
