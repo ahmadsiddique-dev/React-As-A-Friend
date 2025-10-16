2d1da1eb1e32663fc6a27a7efa00e86bdb7dcb10
Ahmad Siddique <asshikrani66@gmail.com>
2025-10-16 20:19:07 +0500

    Login page has been made

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -2,6 +2,7 @@ import Header from "./Header/Header"
 import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
+import Button from './Button'
 import LogoutBtn from "./Header/LogoutBtn";
 
 export {Header, Footer, Container, Logo, LogoutBtn} 
